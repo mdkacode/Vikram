0f863170fa4347d75507f00ee2d3b039095ad46d
Mayank Dwivedi <mayankanildwivedi@gmail.com>
2020-05-16 00:11:31 +0530

    Product Api Completed

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -7,7 +7,7 @@ export interface IproductProps extends Document {
     slug: string;
     createdBy: string;
     updatedBy: string;
-    priceList: [{ mrp: number; sp: number }];
+    priceList: { mrp: number; sp: number };
     units: string;
     defaultSize: string;
     description: string;
@@ -21,9 +21,8 @@ const ProductSchema: Schema = new Schema({
     defaultSize: { type: String, required: true, unique: false },
     slug: { type: String, required: true, unique: true },
     units: { type: String, required: true, unique: true },
-    priceList: { type: String, required: true, unique: false },
-    imagepath: { type: String, required: true, unique: false },
-    imageList: { type: Object, required: true, unique: false },
+    priceList: { type: Object, required: true, unique: false },
+    imageList: { type: Object, required: false, unique: false },
     createdBy: { type: String, required: true, unique: false },
     updatedBy: { type: String, required: true, unique: false },
 }, { timestamps: true });
