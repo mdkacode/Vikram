76fda426953f9fab8555f24a164896240f37c8ba
Vikram Kumar Jaiswal <vikjai@amazon.com>
2020-05-23 23:20:47 +0530

    adding customers

---
+++ src/controllers/userController.ts
@@ -0,0 +1,86 @@
+import { IMAGE_URI, SERVER_IP, NOT_FOUND_IMAGE } from "../util/secrets";
+import fs from "fs";
+import { Types } from "mongoose";
+import { Request, Response, NextFunction } from "express";
+import { infoLog, errorLog } from "../util/loggerInfo";
+import { MasterProductList } from "../models/productModel";
+/**
+ * @description | Add Product with Image, Name & slug
+ * @param req
+ * @param res
+ */
+export const createCustomer = async (req: Request = null, res: Response = null) => {
+
+    const User = new MasterProductList({
+        ...req.body
+    });
+    infoLog("createCustomer", [req.body, req.query]);
+    User.save((err, doc) => {
+        if (err) {
+            errorLog("createCustomer", err, req.method);
+            res.status(500).jsonp({ message: "Field Validation Failed !!", error: err });
+        }
+        else {
+            infoLog("createCustomer => RESPONSE SUCCESS", [req.body, req.query, doc]);
+            res.status(200).jsonp({ message: doc });
+        }
+
+    });
+};
+
+/**
+ * Pass _id to delete the element
+ * @param req |
+ * @param res
+ */
+export const deleteCustomer = async (req: Request = null, res: Response = null, next: NextFunction) => {
+    infoLog("deleteCustomer", [req.body, req.query]);
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+
+        return res.status(500).jsonp({ message: "Body is Not Defined" });
+    }
+    try {
+        const doc = await User.deleteOne({ ...req.body });
+        if (doc.deletedCount > 0) {
+            infoLog("deleteCustomer => SUCCESS", [req.body, req.query, doc]);
+            res.status(200).jsonp({ message: "Item Deleted Successfully", data: doc });
+            next();
+        }
+        else {
+            infoLog("deleteProduct => NO RECORD FOUND", [req.body, req.query, doc]);
+            res.status(204).jsonp({ message: "Item Not Found !!", data: doc });
+        }
+    }
+    catch (error) {
+        errorLog("deleteProduct => PARAMETER ERROR", error, req.body);
+        return res.status(500).jsonp({ mssage: "Please check parameter/s", error });
+    }
+
+
+};
+
+/**
+ * Pass _id to as KEY
+ * @param req | any Body Parameter which wants to get updated
+ * @param res |
+ */
+export const updateCustomer = async (req: Request = null, res: Response = null) => {
+    infoLog("updateCustomer", [req.body, req.query]);
+    User.findOneAndUpdate({ ...req.query }, { ...req.body }, (err: object) => {
+        if (err) {
+            errorLog("updateCustomer => UPDATE FAILED ", err, req.method);
+            return res.status(500).json({ message: "Something went Wrong" });
+        }
+
+    }).then((doc: object) => {
+        if (!doc) {
+            infoLog("updateCustomer", [req.body, req.query, doc]);
+            return res.status(204).json({ message: "Requested Element Not Found !!", item: doc });
+        }
+        else {
+            infoLog("updateCustomer", [req.body, req.query, doc]);
+            return res.status(200).json({ message: "Updated Successfuly!!", item: doc });
+        }
+
+    });
+};
---
+++ src/models/userModel.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+
+export interface User extends Document {
+
+   cIds: String;
+   cName: String;
+   emailId: String;
+   defaultAddress: Array;
+   addresses: String;
+   creationTimeStamp: String;
+   imageLocation: String;
+   isBlackListed: String;
+
+}
+
+
+const UserSchema: Schema = new Schema({
+    cIds: { type: String, required: true, unique: true },   // should be generated at the time of signup by system
+    cName: { type: String, required: true, unique: false },
+    emailId: { type: String, required: true, unique: true },
+    defaultAddress: { type: String, required: false, unique: false },
+    addresses: { type: Array, required: false, unique: false },
+    creationTimeStamp { type: String, required: true, unique: false },
+    imageLocation { type: String, required: false, unique: false }
+    // make default value as 0, if we need to blacklist some customer, we can use this field
+    isBlackListed { type: Number, required: false, unique: false }
+}, { timestamps: true });
+
+
+
+export const User = mongoose.model<User>("User", UserSchema);
