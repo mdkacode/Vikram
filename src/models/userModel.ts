58769d66ab6cdc2e59df03991a4760504a6e2706
Mayank Dwivedi <mayankanildwivedi@gmail.com>
2020-05-24 14:26:14 +0530

    Colen Fix in UserModal

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -3,14 +3,14 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface User extends Document {
 
-   cIds: String;
-   cName: String;
-   emailId: String;
-   defaultAddress: Array;
-   addresses: String;
-   creationTimeStamp: String;
-   imageLocation: String;
-   isBlackListed: String;
+    cIds: String;
+    cName: String;
+    emailId: String;
+    defaultAddress: Array;
+    addresses: String;
+    creationTimeStamp: String;
+    imageLocation: String;
+    isBlackListed: String;
 
 }
 
@@ -21,10 +21,10 @@ const UserSchema: Schema = new Schema({
     emailId: { type: String, required: true, unique: true },
     defaultAddress: { type: String, required: false, unique: false },
     addresses: { type: Array, required: false, unique: false },
-    creationTimeStamp { type: String, required: true, unique: false },
-    imageLocation { type: String, required: false, unique: false }
+    creationTimeStamp: { type: String, required: true, unique: false },
+    imageLocation: { type: String, required: false, unique: false },
     // make default value as 0, if we need to blacklist some customer, we can use this field
-    isBlackListed { type: Number, required: false, unique: false }
+    isBlackListed: { type: Number, required: false, unique: false }
 }, { timestamps: true });
 
 
