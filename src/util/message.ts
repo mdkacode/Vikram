1f8a12589794d64c0f3d927541405a3e4c35f59b
Mayank Dwivedi <mayankdwivedi@funngage.com>
2021-06-16 18:07:34 +0530

    Removed Twillo

--- src/util/message.ts
+++ src/util/message.ts
@@ -1,7 +1,7 @@
-import client from "twilio";
-const accountSid = "AC1adf40fa2851736e7fa7ff1f8911edff";
-const authToken = "023106d65a4c571b38540bff5c509573";
-const messageClient = client(accountSid, authToken);
+// import client from "twilio";
+// const accountSid = "AC1adf40fa2851736e7fa7ff1f8911edff";
+// const authToken = "023106d65a4c571b38540bff5c509573";
+// const messageClient = client(accountSid, authToken);
 import Axios from "axios";
 interface SendMessageProps {
   code: number;
@@ -28,19 +28,19 @@ const sendMessage = async (props: SendMessageProps) => {
   return sendMessage;
 };
 
-const sendWhatsAppMessage = (props: SendMessageProps) => {
-  const { code, userNumber } = props;
-  messageClient.messages
-    .create({
-      from: "whatsapp:+17632963461",
-      body: `${code} is your Mangao code`,
-      to: `whatsapp:${
-        userNumber.includes("+91") ? userNumber : `+91${userNumber}`
-      }`,
-    })
-    .then((message) => console.log("LETTT", message.sid))
-    .catch((e) => {
-      console.log("ERROR", e);
-    });
-};
-export default { sendMessage, sendWhatsAppMessage };
+// const sendWhatsAppMessage = (props: SendMessageProps) => {
+//   const { code, userNumber } = props;
+//   messageClient.messages
+//     .create({
+//       from: "whatsapp:+17632963461",
+//       body: `${code} is your Mangao code`,
+//       to: `whatsapp:${
+//         userNumber.includes("+91") ? userNumber : `+91${userNumber}`
+//       }`,
+//     })
+//     .then((message) => console.log("LETTT", message.sid))
+//     .catch((e) => {
+//       console.log("ERROR", e);
+//     });
+// };
+export default { sendMessage };
