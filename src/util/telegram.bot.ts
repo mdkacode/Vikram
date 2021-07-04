e197b162540040941881ad4439ad6e45c12e1a6a
Mayank Dwivedi <mayankdwivedi@funngage.com>
2021-07-04 21:33:36 +0530

    Bot change

--- src/util/telegram.bot.ts
+++ src/util/telegram.bot.ts
@@ -1,16 +1,15 @@
 import TelegramBot from "node-telegram-bot-api";
 
-const token = "1175820596:AAFVkht4TXOiQOE0aiRbII4iU4DmnYYBjBM";
+const token = "1837187132:AAFK8JOUTU13SfxSLrLW8wIG3ozuQSz9J60";
 
 const bot = new TelegramBot(token, { polling: false });
 
-
-const sendTeleegramNotification = (id: string = "447233341", message: string) => {
-
-    console.log("qwertyuiuytrewqwertyuioiuytrewertyuiuytrewertyui");
-    bot.sendMessage(id, message);
-
+const sendTeleegramNotification = (
+  id: string = "447233341",
+  message: string
+) => {
+  console.log("qwertyuiuytrewqwertyuioiuytrewertyuiuytrewertyui");
+  bot.sendMessage(id, message);
 };
-
 
 export default sendTeleegramNotification;
